7a88e8a08da6376466c1c30918b89353b6cb5d52
Sumit singh <sumit700490@gmail.com>
2025-07-12 11:38:02 +0530

    ok

---
+++ loop-02/question-1.js
@@ -0,0 +1,21 @@
+// write a function that searches for an element in an 
+// array and return the index, if the element is not present just 
+// return -1
+
+
+
+let arr = [4, 2, 0, 10, 8, 30]
+
+function searchElement(arr , el){
+    for(let i=0; i<arr.length;i++){
+        if(arr[i] == el){
+            return i
+        }
+    }
+
+    return -1
+
+}
+
+let result = searchElement(arr,10)
+console.log(result)
